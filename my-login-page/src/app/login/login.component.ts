d1ebc3637c0408f7e9c08cda5c210c7148162ce1
Taner Saydam <taner_and_you@hotmail.com>
2023-09-30 11:03:29 +0300

    .

--- my-login-page/src/app/login/login.component.ts
+++ my-login-page/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  userNameOrEmail: string = "";
+  password:string = "";
 
+  signIn(form:NgForm){
+    if(form.valid){
+      console.log(form);
+      console.log(form.value);
+    }    
+  }
+
+  checkValidation(el:HTMLInputElement){
+    if(!el.validity.valid){
+      el.classList.add("is-invalid"); //form-control is-invalid is-valid
+      el.classList.remove("is-valid");//form-control is-inval
+    }else{
+      el.classList.remove("is-invalid");//form-control is-valid is-invalid
+      el.classList.add("is-valid");//form-control is-valid
+    }
+  }
 }
